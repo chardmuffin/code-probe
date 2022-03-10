4e6f471984e3075efa3b79a76704a34a7e4ca30b
Richard Huffman <richardhuffman96@gmail.com>
2022-03-09 22:03:46 -0500

    added quiz functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,10 +39,11 @@ var questionArray = [{
         "This choice is incorrect."]
 }]
 
-//adjust these for quiz difficulty
 var timeLeft = 75;
 var penalty = 10;
-
+var finalScore = 0;
+var currQuestion = -1;
+var correctAnswerId;
 
 /* ============================================= functions =========================================== */
 
@@ -51,8 +52,11 @@ var welcomeMessage = function() {
 
     var startButtonEl = document.createElement("button");
 
-    //clear any leftover buttons and text if we were just at highscores page
+    //clear any leftover buttons and text if we were just at highscores page, reset vars
     clearContent();
+    timeLeft = 75;
+    currQuestion = -1;
+    finalScore = 0;
 
     //add highscores link and initialized timer to header
     document.querySelector("header").innerHTML = "<span>View Highscores</span><div class='float-right' id='timer'>Time: " + timeLeft + "</div>";
@@ -70,44 +74,110 @@ var welcomeMessage = function() {
     startButtonEl.addEventListener("click", startGame);
 };
 
-//function to begin counter and ask questions
+//function to begin counter and init html for asking questions
 var startGame = function() {
 
-    var correctAnswer;
-
-    //remove welcome message and hide high scores button while taking quiz
+    var unorderedListEl = document.createElement("ul");
+    
+    //remove welcome message and hide high scores button, init prompt message and footer message
     clearContent();
     document.querySelector("header span").remove();
+    document.querySelector(".card-header").innerHTML = "<h2 id='prompt'>If you are seeing this, there was an error</h2>";
+    document.querySelector(".card-footer").innerHTML = "<h3 id='right-wrong'></h3>";
 
-    //init top text for quiz, iterate through questions
-    document.querySelector(".card-header").innerHTML = "<h2 id='question'>Question Goes Here</h2>";
-    for (var i = 0; i < questionArray.length; i++) {
+    //loop 4 times to generate 4 buttons with unique ids and listElements 
+    for (i = 0; i < 4; i++) {
+        var liEl = document.createElement("li");
+        var buttonEl = document.createElement("button");
+        buttonEl.className = "btn";
+        buttonEl.id = String.fromCharCode(65 + i) + ". "; // button ids = "A. ", "B. ", "C. ", "D. "
+        buttonEl.addEventListener("click", nextQuestionHandler);
+        liEl.appendChild(buttonEl);
+        unorderedListEl.appendChild(liEl);
+    };
 
-        //store correct answer, then shuffle answers
-        correctAnswer = questionArray[i].answers[0];
-        console.log(questionArray[i].answers);
-        shuffle(questionArray[i].answers);
-        console.log(questionArray[i].answers);
-    }
+    document.querySelector(".card-body").appendChild(unorderedListEl);
 
-    // run this code every 1000ms
+    //shuffle order of the questions then call the first question
+    shuffle(questionArray);
+    nextQuestionHandler();
+
+    // running this code every 1000ms
     var counter = setInterval(function() {
         timeLeft--;
         document.getElementById("timer").innerHTML = "Time: " + timeLeft;
 
-        if (timeLeft <= 0) {
-            console.log("ran out of time")
+        if (timeLeft <= 0 || currQuestion >= questionArray.length) {
+            console.log("ran out of time or answered all the questions")
             clearInterval(counter);
             displayScore();
         }   
     }, 1000);
 };
 
+//This function is called whenever an answer is clicked
+var nextQuestionHandler = function(event) {
+
+    var correctAnswer;
+    var targetId;
+    var rightWrongEl = document.getElementById("right-wrong")
+
+    currQuestion++;
+
+    //if this is not the first question, display right/wrong message, subtract time if wrong answer, add to score if correct
+    if (event) {
+
+        targetId = event.target.id; // which button did user choose?
+
+        if (correctAnswerId === targetId) {
+            rightWrongEl.textContent = "Correct!"
+            rightWrongEl.style.color = "green";
+            finalScore++;
+        }
+        else {
+            rightWrongEl.textContent = "Wrong!"
+            rightWrongEl.style.color = "red";
+            timeLeft -= 10;
+        }
+
+        rightWrongEl.className = "";
+        setTimeout(function () {
+            rightWrongEl.className = "fade";
+            }, 1000);
+    }
+
+    //if completed all the questions
+    if (currQuestion >= questionArray.length) {
+        return;
+    }
+
+    //display prompt
+    document.getElementById('prompt').textContent = (currQuestion + 1) + ". " + questionArray[currQuestion].prompt;
+
+    //store the correct answer, then shuffle the answers
+    correctAnswer = questionArray[currQuestion].answers[0];
+    shuffle(questionArray[currQuestion].answers);
+
+    //put the answers in the buttons and take note of the correct answer's Id
+    for (i = 0; i < questionArray[currQuestion].answers.length; i++) {
+        var currButton = document.getElementById(String.fromCharCode(65 + i) + ". "); //iterate thru id = "A. ", "B. ", etc.
+        currButton.textContent = currButton.id + questionArray[currQuestion].answers[i];
+        if (currButton.textContent === (currButton.id + correctAnswer)) {
+            correctAnswerId = currButton.id;
+        }
+    }
+
+    console.log((currQuestion + 1) + ". correct answer: " + document.getElementById(correctAnswerId).textContent);
+};
+
 //TODO: request initials, display score
 var displayScore = function() {
-    console.log("displaying your high score. pls enter your initials");
+
+    console.log("Final Score: " + finalScore);
     //TODO: display score
     // request input of initials
+    document.querySelector(".card-header").innerHTML = "";
+    document.querySelector(".card-body").innerHTML = "";
 };
 
 //TODO: Display the high scores
